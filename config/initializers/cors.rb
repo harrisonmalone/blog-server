5cfe8421ec35b9ae2691ccde36d5dbd76dd21b06
Harrison Malone <harrison.malone2@gmail.com>
2021-03-20 13:29:40 +1100

    added url to cors for react snap

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8080', 'www.harrisonmalone.dev'
+    origins 'localhost:8080', 'www.harrisonmalone.dev', 'http://localhost:45678'
 
     resource '*',
       headers: :any,
