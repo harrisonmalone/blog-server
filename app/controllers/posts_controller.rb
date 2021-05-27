b9bfca560d05249627e56c38342ecc6576ac8266
Harrison Malone <harrison.malone@hey.com>
2021-05-27 18:11:15 +1000

    Refactor of posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,28 @@
 require 'open-uri'
 
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show]
+
   def index
+    render json: sorted_by_date_posts
+  end
+
+  def show
+    url = @post.presigned_url(:get)
+    body = URI.open(url).read
+    title =  @post.key.split("-").map { |word| word.capitalize }.join(" ").chomp(".txt")
+    render json: { title: title, body: body, date: @post.data.last_modified }
+  end
+
+  private 
+
+  def set_post
+    @post = BUCKET.objects.find do |obj|
+      obj.key == "#{params[:id]}.txt"
+    end
+  end
+
+  def sorted_by_date_posts
     posts = []
     BUCKET.objects.each do |obj|
       title =  obj.key.split("-").map { |word| word.capitalize }.join(" ").chomp(".txt")
@@ -10,19 +31,8 @@ class PostsController < ApplicationController
       month = date.to_s[5...7]
       posts << { title: title, slug: "#{year}/#{month}/#{obj.key.chomp(".txt")}", date: date }
     end
-    sorted_by_date_posts = posts.sort do |a, b|
+    posts.sort do |a, b|
       b[:date] <=> a[:date]
     end
-    render json: sorted_by_date_posts
-  end
-
-  def show 
-    post = BUCKET.objects.find do |obj|
-      obj.key == "#{params[:id]}.txt"
-    end
-    url = post.presigned_url(:get)
-    body = URI.open(url).read
-    title =  post.key.split("-").map { |word| word.capitalize }.join(" ").chomp(".txt")
-    render json: { title: title, body: body, date: post.data.last_modified }
   end
 end
